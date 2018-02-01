c67f7310487fcd41c77f4270b2ec4d1d1ed461fe
Drew PC <dmorrow@dominiontec.com>
2018-02-01 09:48:40 -0500

    Added Property Converter

---
+++ VS17/PropertyGenerator/PropertyGenerator/MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace PropertyGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.VariablesBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(25)))));
+            this.OutputBox.ForeColor = System.Drawing.Color.White;
+            this.OutputBox.Location = new System.Drawing.Point(508, 31);
+            this.OutputBox.Multiline = true;
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.OutputBox.Size = new System.Drawing.Size(470, 440);
+            this.OutputBox.TabIndex = 1;
+            this.OutputBox.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(505, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Output";
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.BackColor = System.Drawing.Color.DarkGreen;
+            this.GenerateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenerateButton.ForeColor = System.Drawing.Color.DarkOrange;
+            this.GenerateButton.Location = new System.Drawing.Point(12, 477);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(966, 31);
+            this.GenerateButton.TabIndex = 3;
+            this.GenerateButton.Text = "Generate Properties";
+            this.GenerateButton.UseVisualStyleBackColor = false;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Variables";
+            // 
+            // VariablesBox
+            // 
+            this.VariablesBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(25)))));
+            this.VariablesBox.ForeColor = System.Drawing.Color.White;
+            this.VariablesBox.Location = new System.Drawing.Point(12, 31);
+            this.VariablesBox.Multiline = true;
+            this.VariablesBox.Name = "VariablesBox";
+            this.VariablesBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.VariablesBox.Size = new System.Drawing.Size(470, 440);
+            this.VariablesBox.TabIndex = 6;
+            this.VariablesBox.WordWrap = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(990, 520);
+            this.Controls.Add(this.VariablesBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.GenerateButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutputBox);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "MainForm";
+            this.Text = "Property Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox OutputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox VariablesBox;
+    }
+}
+
---
+++ VS17/PropertyGenerator/PropertyGenerator/MainForm.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PropertyGenerator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            string[] lines = null;
+            string[] words = null;
+            string variableName = "";
+            string variableType = "";
+            Boolean inClass = false;
+            OutputBox.Text = "";
+
+            lines = VariablesBox.Text.Split((char)13);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].ToLower().Contains("class"))
+                {
+                    if (lines[i].ToLower().Contains("end"))
+                    {
+                        if (inClass)
+                        {
+                            OutputBox.Text += "}\r\n\r\n";
+                            inClass = false;
+                        }
+                    }
+                    else
+                    {
+                        words = lines[i].Split(' ');
+
+                        for (int j = 0; j < words.Length; j++)
+                        {
+                            if (words[j].ToLower() == "class")
+                            {
+                                if (inClass)
+                                    OutputBox.Text += "}\r\n\r\n";
+
+                                OutputBox.Text += "public Class " + words[j + 1] + "\r\n{\r\n";
+                                inClass = true;
+                            }
+                        }
+                    }
+                }
+
+                if (lines[i].ToLower().Contains("dim"))
+                {
+                    words = lines[i].Trim().Split(' ');
+                    if (words.Length > 3)
+                    {
+                        variableName = words[1];
+                        if (words[3].ToLower() == "new")
+                            variableType = ConvertType(words[4]);
+                        else
+                            variableType = ConvertType(words[3]);
+                    }
+
+                    if (variableName.ToLower().Contains("m_") || variableName.ToLower().Contains("g_"))
+                        variableName = variableName.Substring(1);
+
+                    if (variableName.Length > 2)
+                        variableName = variableName.Substring(0, 2).ToLower() + variableName.Substring(2);
+
+                    OutputBox.Text += "public " + variableType + " " + variableName.Substring(1, 1).ToUpper() + variableName.Substring(2) + "\r\n";
+                    OutputBox.Text += "{" + "\r\n";
+                    OutputBox.Text += "{ get " + variableName + "}\r\n";
+                    OutputBox.Text += "{ set ref " + variableName + "}\r\n";
+                    OutputBox.Text += "}\r\n";
+                    OutputBox.Text += "private " + variableType + " " + variableName + ";\r\n\r\n";
+                }
+            }
+
+            if (inClass)
+            {
+                OutputBox.Text += "}\r\n\r\n";
+                inClass = false;
+            }
+        }
+
+        public string ConvertType(string type)
+        {
+            switch (type)
+            {
+                case "Decimal":
+                    return "decimal";
+                case "Date":
+                    return "DateTime";
+
+                case "String":
+                    return "string";
+
+                case "Byte":
+                    return "byte";
+
+                case "Boolean":
+                    return "bool";
+
+                case "Short":
+                    return "short";
+
+                case "Char":
+                    return "char";
+
+                case "Integer":
+                    return "int";
+
+                case "Long":
+                    return "long";
+
+                case "Single":
+                    return "float";
+
+                case "Double":
+                    return "double";
+
+                default:
+                    return type;
+            }
+        }
+    }
+}
